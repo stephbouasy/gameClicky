bddd38c263ab8670ba9f445c2064af2f0c82e438
sbouasy <stephbouasy@gmail.com>
2020-02-19 18:29:01 -0600

    fixed match function

--- clickgame/src/App.js
+++ clickgame/src/App.js
@@ -1,19 +1,19 @@
 import React, { Component } from "react";
-import MatchCard from "./components/MatchCard";
+import Matched from "./components/Matched";
 import Wrapper from "./components/Wrapper";
 import Title from "./components/Title";
-import matches from "./matchcards.json";
+import matches from "./matched.json";
 import "./App.css";
 
 let correctGuesses = 0;
-let bestScore = 0;
+let topScore = 0;
 let clickMessage = "Save Eleven and the gang from the demogorgons! Click on them once to take them out of the upside down, but click on them twice and they go back!";
 
 class App extends Component {
     state = {
         matches,
         correctGuesses,
-        bestScore,
+        topScore,
         clickMessage
     };
     setClicked = id => {
@@ -21,7 +21,7 @@ class App extends Component {
         const clickedMatch = matches.filter(match => match.id === id);
         if (clickedMatch[0].clicked){
             console.log ("Correct Guesses: " + correctGuesses);
-            console.log ("Best Score: " + bestScore);
+            console.log ("Top Score: " + topScore);
             correctGuesses = 0;
             clickMessage = "The demogorgons have attacked and the upside down has won."
             for (let i = 0 ; i < matches.length ; i++){
@@ -34,9 +34,9 @@ class App extends Component {
             clickedMatch[0].clicked = true;
             correctGuesses++;
             clickMessage = "Great! Don't let the demogorgons win, keep playing.";
-            if (correctGuesses > bestScore){
-                bestScore = correctGuesses;
-                this.setState({ bestScore });
+            if (correctGuesses > topScore){
+                topScore = correctGuesses;
+                this.setState({ topScore });
             }
           matches.sort(function(a, b){return 0.5 - Math.random()});
             this.setState({ matches });
@@ -46,8 +46,8 @@ class App extends Component {
             clickedMatch[0].clicked = true;
             correctGuesses = 0;
             clickMessage = "You beat the upside down! Everyone is safe...for now.";
-            bestScore = 12;
-            this.setState({ bestScore });
+            topScore = 12;
+            this.setState({ topScore });
             for (let i = 0 ; i < matches.length ; i++){
                 matches[i].clicked = false;
             }
@@ -69,12 +69,12 @@ class App extends Component {
                 <h3 className="scoreSummary card-header">
                     Correct Guesses: {this.state.correctGuesses} 
                     <br />
-                    Best Score: {this.state.bestScore} 
+                    Top Score: {this.state.topScore} 
                 </h3>
                 <div className="container">
                 <div className="row">
                 {this.state.matches.map(match => (
-                    <MatchCard
+                    <Matched
                         setClicked={this.setClicked}
                         id={match.id}
                         key={match.id}
